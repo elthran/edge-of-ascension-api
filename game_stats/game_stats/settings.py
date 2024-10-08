59801fa698c1da5d25c75f57e03c6865d6c8a8f7
elthran <jacobbrunner@gmail.com>
2024-10-07 17:09:56 -0700

    clean up

--- game_stats/game_stats/settings.py
+++ game_stats/game_stats/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-9czu(^@rf_pl!xyx2uiy+)%+ks$5)%xssavmzxh=ulbnemyziw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["edgeofascension.pythonanywhere.com "]
 
 
 # Application definition
